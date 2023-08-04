11db7ab4892c43f7d2df54cc8c4e40fc04304703
kathir0206 <110644429+kathir0206@users.noreply.github.com>
2023-08-04 16:05:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AdviceCard from './AdviceCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <AdviceCard/>
+    </div>
+  );
+}
+
+export default App;
